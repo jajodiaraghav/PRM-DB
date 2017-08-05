3f2192e9033caae53b1438abaf6aaacb95aca637
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-08-05 16:18:16 +0530

    Added summary

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
-<?php include_once('partials/header.php'); ?>
+<?php
+include_once('common.php');
+include_once('partials/header.php');
+?>
     <div class="container">
     	<div class="row">
         <div class="col-md-8 col-md-offset-2">
-          <form id="search_form" action="/results" method="get">
-            <div class="input-group input-group-lg">
-              <input type="search" name="q" class="form-control" placeholder="Enter Domain or Protein name or ID">
-              <span class="input-group-btn">
-                <button type="submit" class="btn btn-primary">Search</button>
-              </span>
-            </div>
+          <form action="/results" method="get" class="form-inline">
+            <input type="search" name="q" class="form-control" placeholder="Enter Domain or Protein name or ID">
+            <select class="form-control" name="species" required>
+              <option value="" selected disabled>Species</option>
+              <?php
+              $query = "SELECT DISTINCT Species FROM proteins";
+              $stmt = $dbh->prepare($query);
+              $stmt->execute();
+              while ($row = $stmt->fetch()) {
+                echo "<option value='{$row[0]}'>{$row[0]}</option>";
+              }
+              ?>
+            </select>
+            <button type="submit" class="btn btn-primary">Search</button>
           </form>
           <p>
-            <h4>Load Examples: <a href="/results?q=STAM2">STAM2</a> <a href="/results?q=DOK6">DOK6</a></h4>
+            <h4>Load Examples: <a class="example">STAM2</a> <a class="example">DOK6</a></h4>
           </p>
         </div>
   	  </div>
     </div>
     <?php include_once('partials/footer.php'); ?>
+    <script>
+      $(function() {
+        $('.example').click(function() {
+          $('input[type="search"]').val($(this).text());
+        });
+      });
+    </script>
 	</body>
 </html>
--- results.php
+++ results.php
@@ -4,36 +4,69 @@ include_once('partials/header.php');
 ?>
     <div class="container">
     	<div class="row">
+    		<div class="col-md-10 col-md-offset-1">
+    			<strong>Summary</strong>
+	    		<div class="jumbotron">
+	    			<h5>Search Terms: </h5>
+		    		<div class="inline">		    			
+		    			<ul class="list-unstyled">
+							<li><h5>Proteins Found: </h5></li>
+							<li><h5>Domains Found: </h5></li>
+						</ul>
+						<ul class="list-unstyled">
+							<li><h5>HAL Found: </h5></li>
+							<li><h5>NGS Peptides Found: </h5></li>
+						</ul>
+						<ul class="list-unstyled">
+							<li><h5>3D Structures Found: </h5></li>
+							<li><h5>PWM Found: </h5></li>
+						</ul>
+					</div>
+	    		</div>
+    		</div>
+    	</div>
+    	<div class="row">    		
 	        <div class="col-md-10 col-md-offset-1">
+	        	<strong>Search Results</strong>
 	        	<div class="list-group">
-	        	<?php
-	        	$q = $_GET['q'];
-				$s = isset($_GET['s']) ? mysqli_real_escape_string($_GET['s']) : 0;
+		        	<?php
+		        	$empty = True;
+		        	$q = $_GET['q'];
+		        	$sp = $_GET['species'];
+					$s = isset($_GET['s']) ? mysqli_real_escape_string($_GET['s']) : 0;
 
-				$query = "SELECT * FROM proteins WHERE `Protein_Name` LIKE ? LIMIT ".$s.", 10";
-				$stmt = $dbh->prepare($query);
-				$param = array("%$q%");
-				$stmt->execute($param);
-				while ($row = $stmt->fetch()) {
-	        	?>
-		          	<div class="list-group-item">
-		          		<img src="files/<?=$row['Protein_Name']?>_Logo.png" alt="<?=$row['Protein_Name']?>" width="30%">
-		          		<span class="links">
-		          			<a href="<?=$row['Protein_Name']?>_ELISA.txt">HAL (ELISA Peptides)</a>
-		          			<a href="<?=$row['Protein_Name']?>_SEQ.fa">Top NGS Peptides</a>
-		          		</span>
-		          		<img src="files/<?=$row['Protein_Name']?>_Logo.png" alt="<?=$row['Protein_Name']?>" width="20%">
-		          		<span class="links">
-		          			<strong>Downloads</strong>
-		          			<a href="#">Binders</a>
-		          			<a href="#">PWM</a>
-		          			<a href="#">Peptide Sequence</a>
-		          			<a href="#">Domain Sequence</a>
-		          		</span>
-		          	</div>
-		        <?php } ?>
+					$query = "SELECT * FROM proteins WHERE `Protein_Name` LIKE ? AND `Species`=? LIMIT ".$s.", 10";
+					$stmt = $dbh->prepare($query);
+					$param = array("%$q%", "$sp");
+					$stmt->execute($param);
+					while ($row = $stmt->fetch()) {
+						$empty = False;
+		        	?>
+			          	<div class="list-group-item">
+			          		<img src="files/<?=$row['Protein_Name']?>_Logo.png" alt="<?=$row['Protein_Name']?>" width="30%">
+			          		<span class="links">
+			          			<a href="<?=$row['Protein_Name']?>_ELISA.txt">HAL (ELISA Peptides)</a>
+			          			<span class="seq"><?=$row['HAL']?></span>
+			          			<a href="<?=$row['Protein_Name']?>_SEQ.fa">Top NGS Peptides</a>
+			          			<span class="seq"><?=$row['NGS']?></span>
+			          		</span>
+			          		<img src="files/<?=$row['Protein_Name']?>_Logo.png" alt="<?=$row['Protein_Name']?>" width="20%">
+			          		<span class="links">
+			          			<strong>Downloads</strong>
+			          			<a href="#">Binders</a>
+			          			<a href="#">PWM</a>
+			          			<a href="#">Peptide Sequence</a>
+			          			<a href="#">Domain Sequence</a>
+			          		</span>
+			          	</div>
+			        <?php } if ($empty) { ?>
+			        	<div class="list-group-item text-center">Nothing Found!</div>
+			        <?php } ?>
 	        	</div>
 	        </div>
+	    </div>
+	    <?php if (!$empty) { ?>
+	    <div class="row">
 	        <div class="col-md-4 col-md-offset-4">
 	        	<nav aria-label="...">
 					<ul class="pager">
@@ -44,6 +77,7 @@ include_once('partials/header.php');
 				</nav>
 	        </div>
   		</div>
+  		<?php } ?>
     </div>
     <?php include_once('partials/footer.php'); ?>
 	</body>
